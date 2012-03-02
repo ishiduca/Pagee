1b1f361d6f415456a30a181c19556fad04599d4a
ishiduca <ishiduca@gmail.com>
2012-03-02 18:09:43 +0900

    change index.js

--- public/js/index.js
+++ public/js/index.js
@@ -6,16 +6,16 @@ function Revolver (data) {
     this.dom.listLast = tags(_dom.list, 'li').length - 1;
 
     var that = this;
-    addEvent(window, 'resize', function () { that.onResize(); });
+    addEvent(window, 'resize', function () { that.__onResize__(); });
 
-    return this.pos(this.focus * this.pitch).onResize();
+    return this.__pos__(this.focus * this.pitch).__onResize__();
 }
 (function (rp) {
-    rp.setBrowserWidth = function () {
+    rp.__setBrowserWidth__ = function () {
                     this.browserWidth = getBrowserWidth();
                     return this;
     };
-    rp.setWidth = function () {
+    rp.__setWidth__ = function () {
                     var dom      = this.dom,
                         domWidth = this.browserWidth - (this.dom.widthMargin * 2);
 
@@ -25,9 +25,19 @@ function Revolver (data) {
 
                     return this;
     };
-    rp.onResize = function () {
-                    return this.setBrowserWidth().setWidth();
+    rp.__onResize__ = function () {
+                    return this.__setBrowserWidth__().__setWidth__();
     };
+    rp.__pos__ = function (_pos) {
+                    var _dom = this.dom;
+
+                    _pos = _pos || 0;
+
+                    _dom.list.style.top = (_dom.defaultTop - _pos) + 'px';
+
+                    return this;
+    };
+
     rp.roll = function (addFocus) {
                     if (! this.scrollState) {
                         var _focus = this.focus,
@@ -54,11 +64,11 @@ function Revolver (data) {
                                 if ((isUp) ? (start < finish) : (start > finish)) {
                                     clearInterval(that.scrollState);
                                     delete that.scrollState;
-                                    that.pos(finish);
+                                    that.__pos__(finish);
                                     return;
                                 }
                                 start = (isUp) ? (start - that.scroll.pitch) : (start + that.scroll.pitch);
-                                that.pos(start);
+                                that.__pos__(start);
                             }, this.scroll.interval);
 
                             this.focus = _focus;
@@ -66,16 +76,7 @@ function Revolver (data) {
                     }
                     return this;
     };
-    rp.pos = function (_pos) {
-                    var _dom = this.dom;
-
-                    _pos = _pos || 0;
-
-                    _dom.list.style.top = (_dom.defaultTop - _pos) + 'px';
-
-                    return this;
-    };
-    rp.onSelect = function (func) {
+    rp.fire = function (func) {
                     var link = tags(this.dom.list, 'a')[this.focus];
 
                     if ((! isUndefined(func)) && typeof func === 'function') {
@@ -116,7 +117,7 @@ addEvent(window, 'load', function () {
 
         (new Hotkey)
             .add(qw('space enter'), function () {
-                revolver.onSelect(function (link /*, now*/) {
+                revolver.fire(function (link /*, now*/) {
                         link.style.color = '#ff8800';
                         setTimeout(function () { document.location = link.href; }, 250);
                 });
